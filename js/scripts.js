10e31f4bf5a3f4d6cf489644e1f9171381cef47b
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-06-17 11:02:53 -0700

    Add pseudocode for JS form behavior

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $(".initially-hidden").hide();
+
+    //INSERT NAME INTO A SPAN IN RESULTS
+
+    //IF (all any)
+      //please try again, or show all
+
+    // ELSE IF (exhilirating)
+      //if (cheap)
+        //DENALI
+      //else if moderate
+        //MACHU PICCHU
+      //else
+        //RIO
+
+    //ELSE IF cold
+      //DENALI
+
+    //ELSE IF (Automobile || train)
+      //SAVANNAH
+
+    //ELSE IF (relaxing)
+      //PUNTO CANA
+
+    //ELSE IF interesting
+      //IF cheap
+        //CAMBODIA
+      //else
+        //SANTORINI
+
+
+
+
+  })
+});
+
+
+/*
+
+hot
+temperate
+
+expensive
+moderate
